dbf1648ccb9aed81fd8d61235826d20aff3f7c68
Rae Alejandrino <raealejandrino@gmail.com>
2021-12-30 15:51:59 -0500

    Added scrolling feature buttons to status rows

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,26 @@
 let sidebar = document.querySelector('.sidebar');
 let btn = document.querySelector('#btn');
 
+let currentScrollPosition = 0;
+let scrollAmount = 320;
+
+const sCont = document.querySelector("#statusCardRowCont");
+const hScroll = document.querySelector("#statusCardRow");
+
+
+let maxScroll = -sCont.offsetWidth + hScroll.offsetWidth;
+
+const scrollHorizontally = val => {
+    currentScrollPosition += (val * scrollAmount);
+
+    if (currentScrollPosition > 0) {
+        currentScrollPosition = 0;
+    }
+
+    
+    sCont.style.left = currentScrollPosition + "px";
+}
+
 btn.addEventListener("click", () => {
     sidebar.classList.toggle('open');
     btnIconChange();
